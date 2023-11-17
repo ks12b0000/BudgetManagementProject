983df14c87cdfd686667827a504716e69e2d56e1
ks12b0000 <ks12b0000@gmail.com>
2023-11-17 18:12:47 +0900

    feat: 오늘의 지출 안내 스케줄러 추가 #14

--- src/main/java/com/wanted/budgetmanagement/api/expenditure/service/ExpenditureSchedulerService.java
+++ src/main/java/com/wanted/budgetmanagement/api/expenditure/service/ExpenditureSchedulerService.java
@@ -1,5 +1,6 @@
 package com.wanted.budgetmanagement.api.expenditure.service;
 
+import com.wanted.budgetmanagement.api.expenditure.dto.ExpenditureGuideResponse;
 import com.wanted.budgetmanagement.api.expenditure.dto.ExpenditureRecommendResponse;
 import com.wanted.budgetmanagement.domain.user.entity.User;
 import com.wanted.budgetmanagement.domain.user.repository.UserRepository;
@@ -39,5 +40,28 @@ public class ExpenditureSchedulerService {
             mailSender.send(mailMessage);
         }
 
+    }
+
+    @Transactional
+//    @Scheduled(initialDelay = 1000 ,fixedDelay = 60 * 60 * 1000)
+    @Scheduled(cron = "0 0 8 * * ?", zone = "Asia/Seoul")
+    public void expenditureGuideScheduler() {
+        List<User> users = userRepository.findAll();
+        for (int i = 0; i < users.size(); i++) {
+            String message = "오늘 카테고리별 지출 금액: ";
+            User user = users.get(i);
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            ExpenditureGuideResponse response = expenditureService.expenditureGuide(user);
+            mailMessage.setTo(user.getEmail());
+            mailMessage.setSubject("안녕하세요! BudgetManagement 서비스 입니다. 오늘의 지출 내역을 안내해드려요!");
+            for (int j = 0; j < response.getGuideList().size(); j++) {
+                message += " 카테고리: " + response.getGuideList().get(j).getCategory().getName()  +
+                        " 오늘 지출 금액: " + response.getGuideList().get(j).getTodayExpenditureAmount() +
+                        " 오늘 적정 지출 금액: " + response.getGuideList().get(j).getTodayAppropriateExpenditureAmount() +
+                        " 위험도: " + response.getGuideList().get(j).getRisk();
+            }
+            mailMessage.setText(message + " 오늘 총 지출 금액: " + response.getTotalAmount());
+            mailSender.send(mailMessage);
+        }
     }
 }
